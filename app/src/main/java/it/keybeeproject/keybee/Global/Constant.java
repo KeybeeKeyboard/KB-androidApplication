e6179cf6e17880b88ab49362e6f0ca7c9e015313
NI80ON <marco.papalia@ing.com>
2025-02-06 16:05:43 +0100

    updated urls

--- app/src/main/java/it/keybeeproject/keybee/Global/Constant.java
+++ app/src/main/java/it/keybeeproject/keybee/Global/Constant.java
@@ -8,8 +8,8 @@ public class Constant {
 	public static final String URL_KEYBEE_SPEED_CONTEST = "https://keybeekeyboard.com/speedcontest.html";
 	public static final String URL_FACEBOOK = "https://www.facebook.com/KeybeeKeyboard";
 	public static final String URL_TWITTER = "https://twitter.com/KeybeeKeyboard";
-	public static final String URL_LINKEDIN = "https://www.linkedin.com/company/keybee";
-	public static final String URL_GITHUB = "https://github.com/KeybeeKeyboard/androidApplication";
+	public static final String URL_LINKEDIN = "https://www.linkedin.com/company/keybeekeyboard";
+	public static final String URL_GITHUB = "https://github.com/KeybeeKeyboard/KB-androidApplication";
 	public static final String URL_DONATE = "https://keybeekeyboard.com/donate.html";
 	public static final String SKU = "all_theme_monthly_subscription";
 	public static final String KEY_IAB = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArUh1BpWPJ/UQQkZUAxasr+Nq4yZpWXqEZv8Eq2HSvhi2VpASPOcSHxagW35hzqVzvuHBWzlvQ1F/76O5WxjDvc66SD8CrD5KzRXnvddkVwETg7yfjMpOMAfOudDlc8lv7Rtk5B9TU2omJoiO9h4f3zs6B5BSq4lZWIUh7tC8+3scB/NBP0l3JntQVYcQcFPScVOQcnroBNNsz8J7A0+9NsSKppLBfBH9OcZM0Uwb4H9azDblOkSrefCPMrB6ywMmpYNZTGo7IYh2scYzExA/1K44nkbdEfOQcUK3aqi1T/BnuJaxO7DNZ8BC58r3s0gCMBXxAq7CKQuxFc6fwuMsTwIDAQAB";
